eb8d377da257a948a7fc3b650a84f46c059e548f
rifkisetiawan0101 <rifki.setiawan0101@gmail.com>
2025-07-21 02:22:58 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-      remotePatterns: [
-          {
-              protocol: 'https',
-              hostname: 'ubyihlbdtuuohxzdmkxl.supabase.co',
-              port: '',
-              pathname: '/storage/v1/object/public/**',
-          },
-      ],
-      unoptimized: true,
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ubyihlbdtuuohxzdmkxl.supabase.co',
+                port: '',
+                pathname: '/storage/v1/object/public/**',
+            },
+        ],
+        unoptimized: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
