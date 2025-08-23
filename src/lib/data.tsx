ee72a51d111d2f6e6981e978622a5baab96f2506
rifkisetiawan0101 <rifki.setiawan0101@gmail.com>
2025-08-23 17:10:51 +0700

    Update data.tsx

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -3,7 +3,7 @@ export const personalInfo = {
     roles: ["Game", 2000, "Fullstack", 1500],
     bio: "Digital Multimedia Engineering student who specializes in game and full-stack website development. Experienced in the project development cycle from idea to deployment, proven by the success of deploying several game and websites, and also becoming the Top 10 of TSA Game Fest 2024 competition. Skilled in applying modern development principles to create scalable and efficient applications.",
     email: "rifki.setiawan0101@gmail.com",
-    cvLink: "https://ubyihlbdtuuohxzdmkxl.supabase.co/storage/v1/object/public/rstiawann-assets/CV/_CV-Rifki-Setiawan-English.pdf",
+    cvLink: "https://ubyihlbdtuuohxzdmkxl.supabase.co/storage/v1/object/public/rstiawann-assets/CV/CV_Rifki%20Setiawan_Game-Programmer_English.pdf",
     socials: {
         linkedin: "https://linkedin.com/in/rifki-setiawan0101",
         github: "https://github.com/rifkisetiawan0101",
